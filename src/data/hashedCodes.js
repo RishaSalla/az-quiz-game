b2bd51abf705513289c3ed0c1de86346b68c6089
RishaSalla <salam.kedan@gmail.com>
2026-01-26 23:26:10 +0100

    Create hashedCodes.js

---
+++ src/data/hashedCodes.js
@@ -0,0 +1,11 @@
+export const ACCESS_DATA = {
+  "game": "",
+  "last_update": "2026-01-12 01:57:20.761681",
+  "total_count": 3000,
+  "valid_hashes": [
+    "f964a1522ecbc71727587ed57d4e52f9a4a6a536189948520ddf277fd66848e4",
+    "1ae126752e417ad5f39433bcb256f27c44299bfd297b2575918620c89c7db97c",
+    "7d5346ecb0807f2993c1938dca4ac01f7f4c5bf456d8b0afb5facbc1ee69ccbc"
+    // يمكنك إضافة بقية الأكواد هنا
+  ]
+};
