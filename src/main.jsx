a2a381883974f1d8eb2fc9896d9d7fa6d33dd573
RishaSalla <salam.kedan@gmail.com>
2026-02-18 01:11:33 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './App.jsx';
 import './index.css';
 
+// في React، يفضل ترك مهمة التوجيه لملف App.jsx 
+// لضمان عدم حدوث تداخل يسبب الصفحة البيضاء
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* أضفنا basename ليحترم اسم المستودع ولا يخرج منه */}
-    <BrowserRouter basename="/az-quiz-game">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
